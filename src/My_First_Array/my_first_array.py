913fe16d45dfd2b61439a6754549c4d0ffe6135b
xintongc2010-cmd <xintongc2010@gmail.com>
2026-06-18 23:22:10 +0400

    I learnt a few other functions that can be applied to arrays

--- src/My_First_Array/my_first_array.py
+++ src/My_First_Array/my_first_array.py
@@ -5,3 +5,39 @@ def main():
 
 
 main()
+
+
+def my_create():
+    array = [10, 20, 30, 40, 50]
+    print(array)
+
+
+my_create()
+
+
+def my_insert():
+    array = [10, 20, 30, 40, 50]
+    array.insert(0, 50)
+    array.insert(1, 4)
+    print(array)
+
+
+my_insert()
+
+
+def my_remove():
+    array = [10, 20, 30, 40, 50]
+    array.remove(30)
+    print(array)
+
+
+my_remove()
+
+
+def my_update():
+    array = [10, 20, 30, 40, 50]
+    array.update(2, 70)
+    print(array)
+
+
+my_update()
