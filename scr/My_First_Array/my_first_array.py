4c4159e07e97b0c18f6b4fd907176b190ea2c559
xintongc2010-cmd <xintongc2010@gmail.com>
2026-06-20 20:44:39 +0400

    I made the arrays into highly typed code

--- scr/My_First_Array/my_first_array.py
+++
@@ -1,7 +0,0 @@
-def main():
-    temperatures = [None] * 31
-    temperatures[0] = 15
-    print(temperatures[0])
-
-
-main()
--- src/My_First_Array/my_first_array.py
+++ src/My_First_Array/my_first_array.py
@@ -1,43 +1,34 @@
-def main():
-    temperatures = [None] * 31
-    temperatures[0] = 15
-    print(temperatures[0])
-
-
-main()
 
 
 def my_create():
-    array = [10, 20, 30, 40, 50]
+    array: list[int] = [10, 20, 30, 40, 50]
     print(array)
 
 
-my_create()
-
-
 def my_insert():
-    array = [10, 20, 30, 40, 50]
+    array: list[int] = [10, 20, 30, 40, 50]
     array.insert(0, 50)
     array.insert(1, 4)
     print(array)
 
 
-my_insert()
-
-
 def my_remove():
-    array = [10, 20, 30, 40, 50]
+    array: list[int] = [10, 20, 30, 30, 40, 50, ]
     array.remove(30)
     print(array)
 
 
-my_remove()
-
-
 def my_update():
-    array = [10, 20, 30, 40, 50]
-    array.update(2, 70)
+    array: list[int] = [10, 20, 30, 40, 50]
+    array[1] = 90
     print(array)
 
 
-my_update()
+def main():
+    my_create()
+    my_insert()
+    my_remove()
+    my_update()
+
+
+main()
