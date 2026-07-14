04c5c1863bda4d0ff7295c60109d5a8579990e71
xintongc2010-cmd <xintongc2010@gmail.com>
2026-07-14 20:39:05 +0800

    I added a bubble sort variant using break

--- src/sorting_algorithms/bubble_sort.py
+++ src/sorting_algorithms/bubble_sort.py
@@ -1,3 +1,4 @@
+
 def bubble_sort(array_to_sort: list[int]):
     length = len(array_to_sort)
     did_it_swap = True
@@ -13,13 +14,11 @@ def bubble_sort(array_to_sort: list[int]):
                 did_it_swap = True
 
 
-'''
 def main():
     bubble_sort(array_to_sort=[10, 30, 60])
 
 
 main()
-'''
 
 
 def my_swap(list_to_swap: list[int], index_to_swap_from: int, index_to_swap_with: int):
@@ -35,4 +34,62 @@ def main():
     print(my_new_list)
 
 
-main()
+def my_swap(list_to_swap: list[int], index_to_swap_from: int, index_to_swap_with: int):
+    new_value = list_to_swap[index_to_swap_from]
+    list_to_swap[index_to_swap_from] = list_to_swap[index_to_swap_with]
+    list_to_swap[index_to_swap_with] = new_value
+    return list_to_swap
+
+
+def bubble_sort(array_to_sort: list[int]):
+    length = len(array_to_sort)
+    did_it_swap = True
+    while did_it_swap == True:
+        did_it_swap = False
+
+        for index in range(0, length-1):
+            element1 = array_to_sort[index]
+            element2 = array_to_sort[index+1]
+            if element1 > element2:
+                print(f'[To Swap] — {element1}, and {element2}')
+                array_to_sort = my_swap(array_to_sort, index, index+1)
+                did_it_swap = True
+
+        print(f"[End of For Loop]Array looks like this now: {array_to_sort}")
+        print(
+            f"Does the array still need to be sorted / checked one last time? {did_it_swap}")
+    return array_to_sort
+
+
+bubble_sort([2, 7, 5, 10, 8, 9])
+
+
+def my_swap(list_to_swap: list[int], index_to_swap_from: int, index_to_swap_with: int):
+    new_value = list_to_swap[index_to_swap_from]
+    list_to_swap[index_to_swap_from] = list_to_swap[index_to_swap_with]
+    list_to_swap[index_to_swap_with] = new_value
+    return list_to_swap
+
+
+def main():
+    my_list = [2, 10, 14, 5, 9]
+    print(f"Unsorted list: {my_list}")
+    swapped_list = bubble_sort(array_to_sort=my_list)
+    print(f"Sorted list: {swapped_list}")
+
+
+def bubble_sort_variant_amy(array_to_sort: list[int]):
+    length = len(array_to_sort)
+    did_it_swap = True
+    while did_it_swap == True:
+        did_it_swap = False
+
+        for index in range(0, length-1):
+            element1 = array_to_sort[index]
+            element2 = array_to_sort[index+1]
+            if element1 > element2:
+                print(element1, element2)
+                array_to_sort = my_swap(array_to_sort, index, index+1)
+                did_it_swap = True
+                if element1 == element2:
+                    break
